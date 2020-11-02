0d4cf37ce0e44528264c9472649cf4301d712ae0
Tauran <tauran@Taurans-MacBook.hitronhub.home>
2020-11-01 20:53:05 -0700

    Add interface

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ function createWindow () {
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+	app.quit()
 })
 
 app.on('activate', () => {
