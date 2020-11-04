fccd34451abf7b7b48eedb0cd9a3fead31e77d44
thirstyice <tauranwood@gmail.com>
2020-11-04 12:11:57 -0700

    Added dropdown functionality

--- tracker.js
+++ tracker.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const {ipcRenderer} = require('electron');
 var startDate
 var csv = 'Type,Gauge,Metal,Size,Time per'
+var configuration;
 
 function openSettings() {
 	ipcRenderer.sendSync('openSettings');
@@ -57,8 +58,85 @@ function endDay() {
 	switchButtonToStartDay()
 }
 
+function createElement(element, id, eventName, responder) {
+	var node = document.createElement(element);
+	var idAttribute = document.createAttribute("id");
+	idAttribute.value = id;
+	node.setAttributeNode(idAttribute);
+	if (eventName != null) {
+		var eventAttribute = document.createAttribute(eventName);
+		eventAttribute.value = responder;
+		node.setAttributeNode(eventAttribute);
+	}
+	return node;
+}
+
+function populateDropdown(dropdown, children) {
+	if (document.getElementById( dropdown + "Selector") == null) {
+		document.getElementById("dropdowns").appendChild(
+			createElement("select", dropdown + "Selector", "onchange", "selected(this.id)")
+		);
+	}
+	var selector = document.getElementById(dropdown + "Selector");
+	while (selector.hasChildNodes()) {
+		selector.removeChild(selector.firstChild);
+	}
+	var i;
+	for (i = 0; i < children.length; i++) {
+		var id = children[i];
+		if (dropdown == 'type') {
+			id = i;
+		}
+		var node = createElement("option", id, null, null);
+		node.appendChild(document.createTextNode(children[i]));
+		selector.appendChild(node);
+	}
+	selected(dropdown + "Selector");
+}
+
+function initDropdowns() {
+	var confFilePath = ipcRenderer.sendSync('getConfFilePath');
+	var flags = 'w+'
+	if (fs.existsSync(confFilePath)) {
+		flags = 'r+'
+	}
+	var confFile = fs.readFileSync( confFilePath, {encoding:'utf-8', flag:flags} );
+	configuration = JSON.parse(confFile);
+
+	populateDropdown('tasks', configuration.tasks);
+
+
+	var types = [];
+	var i;
+	for (i=0; i<configuration.items.length; i++) {
+		types[i] = configuration.items[i].type;
+	}
+	populateDropdown('type', types)
+}
+
+function selected(dropdownId) {
+	var options = document.getElementById(dropdownId).options;
+	var selectedId = options[options.selectedIndex].id;
+	if (dropdownId == "typeSelector") {
+		var confFilePath = ipcRenderer.sendSync('getConfFilePath');
+		var flags = 'w+'
+		if (fs.existsSync(confFilePath)) {
+			flags = 'r+'
+		}
+		var confFile = fs.readFileSync( confFilePath, {encoding:'utf-8', flag:flags} );
+		configuration = JSON.parse(confFile);
+
+		populateDropdown('gauge', configuration.items[selectedId].gauge);
+		populateDropdown('metal', configuration.items[selectedId].metal);
+		populateDropdown('size', configuration.items[selectedId].size);
+	}
+}
+
+
+
 // init dropdowns from config files
 // init buttons
 window.onload = function() {
+	initDropdowns();
 	switchButtonToStartDay()
 }
